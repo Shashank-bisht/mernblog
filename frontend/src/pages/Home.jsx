21992a587aaa5f97491a218910dcd6a2cbb32847
Shashank Bisht <105352161+Shashank-bisht@users.noreply.github.com>
2023-12-28 22:02:55 +0530

    added loader

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import HomePosts from '../components/HomePosts'
 import Navbar from '../components/Navbar'
+import Loader from '../components/Loader'
 import Footer from '../components/Footer'
 import { URL } from '../url'
 import { useLocation } from 'react-router-dom'
@@ -9,11 +10,20 @@ const Home = () => {
   // getting search property from useLocation()
   const {search}= useLocation()
   const [posts, setPosts] = useState([])
+  const [loader, setloader] = useState(false)
+  const [noResults, setNoResults] = useState(false)
    const fetchPosts = async()=>{
+    setloader(true)
     try{
     const res = await axios.get(URL+"/api/post/"+search)
     // console.log(res.data)
     setPosts(res.data)
+    if(res.data.length === 0){
+      setNoResults(true)
+    }else{
+      setNoResults(false)
+    }
+    setloader(false)
     }catch(err){
   console.error(err)
     }
@@ -21,14 +31,15 @@ const Home = () => {
    useEffect(()=>{
    fetchPosts()
    },[search])
+   //The reason search is passed inside the dependency array [search] in the useEffect is to specify that the effect should run whenever the search value changes. 
   return (
     <>
     <Navbar/>
      <div className='px-8 md:px-[200px] min-h-[80vh]'>
       {/* For each post, a HomePosts component is rendered, passing the post data as a prop and a key to uniquely identify each post component. */}
-    {posts.map((post)=>(
+    {loader?<div className='h-[40vh] flex justify-center items-center'><Loader/></div>:!noResults?posts.map((post)=>(
       <HomePosts key={post._id} post={post}/>
-    ))}
+    )):<h3 className='text-center font-bold'>No posts available</h3>}
     </div>
     <Footer/>
     </>
